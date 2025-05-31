d0e99c4fee169853a32f0bc2a80d00ae37dcd0b8
minhanh3110 <lehasong@gmail.com>
2025-05-31 11:43:54 +0700

    Cập nhật cấu hình cho Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nuoi_cay_mo.db'
@@ -44,4 +45,5 @@ def them_mau():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
